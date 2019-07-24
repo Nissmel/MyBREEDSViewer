6dd30037e12f1f787404fc2369a4b4795e3fa8c6
Łukasz <ukasz@192.168.0.67>
2019-07-24 17:27:38 +0200

    New buttons and breed show possibility

--- MySQL viewer/src/myView/View.java
+++ MySQL viewer/src/myView/View.java
@@ -2,10 +2,18 @@ package myView;
 
 import java.sql.*;
 
-public class View extends Window{
+import com.mysql.cj.util.TestUtils;
 
-	public static void connect(String URL, String user, String password, String query, String breed_name)
+
+public class View extends Window{
+	
+	static int currentBreed = 2;
+	static int count = 1;
+	
+
+	public static void connect(String URL, String user, String password, String query)
 	{
+		populateText();
 		try {
 			Connection myConn = DriverManager.getConnection(URL, user, password);
 			
@@ -13,16 +21,60 @@ public class View extends Window{
 			
 			ResultSet myRs = myStmt.executeQuery(query);
 			
+			count =0;
 			while(myRs.next())
 			{
-				System.out.println(myRs.getString(breed_name));
-	
+				breedInfo[count]=(myRs.getString(Integer.toString(currentBreed)));	
+				Window.setBreed();
+				count++;
 			}
 			
 		}
 		catch (Exception exc) {
 			exc.printStackTrace();
 		}
+	}
+	
+	public static void changeBreed(boolean change)
+	{
+		if(change)
+		{
+			if(currentBreed<17)
+				currentBreed++;
+		}
+		
+		if(!change)
+		{
+			if(currentBreed>1)
+				currentBreed--;
+		}
+		
+		connect(URL, user, password, query);
+		
+		for(int i=0;i<16;i++)
+		{
+			//System.out.println(breedInfo[i]);	
+		}
+	}
+	
+	public static void populateText()
+	{
+		text[0]="Breed name:";
+		text[1]="Weight:";
+		text[2]="Life Span:";
+		text[3]="Size:";
+		text[4]="Popularity:";
+		text[5]="Intelligence";
+		text[6]="Sensitivity Level:";
+		text[7]="Easy To Groom:";
+		text[8]="Drooling Potential:";
+		text[9]="General Health:";
+		text[10]="Tolerates Being Alone:";
+		text[11]="Easy To Train:";
+		text[12]="Prey Drive:";
+		text[13]="Wanderlust Potential:";
+		text[14]="Tendency To Bark:";
+		text[15]="Potentail For Mouthiness:";
 	}
 
 
--- MySQL viewer/src/myView/Window.java
+++ MySQL viewer/src/myView/Window.java
@@ -6,27 +6,56 @@ import javax.swing.JFrame;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import javax.swing.JTextPane;
 import javax.swing.border.LineBorder;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.Font;
+
+
 import javax.swing.JLayeredPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import java.awt.CardLayout;
+import javax.swing.JLabel;
 
 
-public class Window {
 
-	private JFrame frame;
-	public JPanel addBreed;
-	public JPanel viewBreed;
 
+public class Window  {
+
+	public static void setBreed()
+	{
+		for(int i=0; i<16;i++) {
+			breedLabels[i].setText(breedInfo[i]);
+			breedLabels[i].setBounds(600,100+i*30,300, 100);
+			breedLabels[i].setFont(new Font("Verdana", Font.PLAIN, 20));
+			viewBreed.add(breedLabels[i]);
+		}
+	}
 	
+	public static void setText()
+	{
+		for(int i=0; i<16;i++) {
+			textLabels[i].setText(text[i]);
+			textLabels[i].setBounds(300,100+i*30,300, 100);
+			textLabels[i].setFont(new Font("Verdana", Font.PLAIN, 20));
+			viewBreed.add(textLabels[i]);
+		}
+	}
+	
+	
+	public static String URL = "jdbc:mysql://localhost:3306/chooseyourpuppy";
+	public static String user = "root";
+	public static String password = "";	
+	public static String query = "select * from breeds";
+	
+	static String [] breedInfo = new String[16];
+	static String [] text = new String[16];
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -40,13 +69,8 @@ public class Window {
 			}
 		});		
 		
-		String URL = "jdbc:mysql://localhost:3306/chooseyourpuppy";
-		String user = "root";
-		String password = "";	
-		String query = "select * from breeds";
-		
-		View.connect(URL, user, password, query, "Akita");
 
+		View.connect(URL, user, password, query);
 
 	}
 
@@ -55,8 +79,16 @@ public class Window {
 		initialize();
 	}
 	
+
+	private JFrame frame;
+	public JPanel addBreed;
+	public static JPanel viewBreed;
+	public JPanel menu;
+	public static JLabel[] textLabels;
+	public static JLabel[] breedLabels;
 	
-	private  void initialize() {
+public  void initialize() {
+		
 		
 		final int WIDTH = 1280, HEIGHT = 720;
 		frame = new JFrame();
@@ -69,78 +101,90 @@ public class Window {
 		frame.setTitle("MyBREEDS Viewer");
 		frame.setResizable(false);
 		frame.setVisible(true);
+
 		
-		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setBounds(0, 0, WIDTH, HEIGHT);
-		frame.getContentPane().add(layeredPane);
-		layeredPane.setLayout(null);
+		
+		//header
+		JPanel red = new JPanel();
+		red.setBounds(400, 0, 888, 80);
+		frame.getContentPane().add(red);
+		red.setBackground(new Color(204, 0, 0));
+		red.setLayout(null);
 		
 		JPanel darkGrey = new JPanel();
+		darkGrey.setBounds(0, 0, 387, 80);
+		frame.getContentPane().add(darkGrey);
 		darkGrey.setBackground(new Color(51, 51, 51));
-		darkGrey.setBounds(0, 0, 393, 80);
-		layeredPane.add(darkGrey);
 		darkGrey.setLayout(null);
 		
-		JTextPane txtpnChoose = new JTextPane();
-		txtpnChoose.setForeground(new Color(255, 255, 255));
+		JLabel txtpnChoose = new JLabel();
+		txtpnChoose.setForeground(new Color(240, 240, 240));
 		txtpnChoose.setBounds(56, 11, 367, 63);
 		txtpnChoose.setFont(new Font("Verdana", Font.BOLD, 46));
 		txtpnChoose.setText("Choose your");
 		txtpnChoose.setBackground(null);
 		darkGrey.add(txtpnChoose);
 		
-		JPanel red = new JPanel();
-		red.setBounds(400, 0, 880, 80);
-		layeredPane.add(red);
-		red.setBackground(new Color(204, 0, 0));
-		red.setLayout(null);
-		
-		JTextPane txtpnPuppy = new JTextPane();
+		JLabel txtpnPuppy = new JLabel();
 		txtpnPuppy.setBounds(5, 11, 166, 63);
-		txtpnPuppy.setForeground(new Color(255, 255, 255));
-		txtpnPuppy.setFont(new Font("Verdana", Font.BOLD, 46));
+		txtpnPuppy.setForeground(new Color(240, 240, 240));
+		txtpnPuppy.setFont(new Font("Nunito-Bold", Font.BOLD, 46));
 		txtpnPuppy.setText("puppy");
 		txtpnPuppy.setBackground(null);
 		red.add(txtpnPuppy);
 		
 		
+
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(0, 0, WIDTH, HEIGHT);
+		frame.getContentPane().add(layeredPane);
+		layeredPane.setLayout(new CardLayout(0, 0));
+		
+		JButton btnMenu = new JButton("Back to menu");
+		btnMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				layeredPane.removeAll();
+				layeredPane.add(menu);
+				layeredPane.repaint();
+				layeredPane.revalidate();
+			}
+		});
+		btnMenu.setForeground(Color.WHITE);
+		btnMenu.setBackground(new Color(51, 51, 51));
+		btnMenu.setFont(new Font("Verdana", Font.BOLD, 18));
+		btnMenu.setBounds(660, 20, 180, 40);
+		btnMenu.setBorderPainted(false);
+		btnMenu.setFocusPainted(false);
+		red.add(btnMenu);
 		
 		
-		viewBreed = new JPanel();
-		viewBreed.setBounds(0, 0, 1274, 691);
-		layeredPane.add(viewBreed);
-		viewBreed.setLayout(null);
-		viewBreed.setVisible(false);
-		viewBreed.setBackground(Color.WHITE);
 		
-		JButton prevBreed = new JButton("<");
-		prevBreed.setFont(new Font("Verdana", Font.PLAIN, 78));
-		prevBreed.setBounds(34, 274, 100, 100);
-		viewBreed.add(prevBreed);
-		prevBreed.setVisible(true);
-		prevBreed.setBorder(new LineBorder(Color.WHITE));
-		prevBreed.setBackground(Color.WHITE);
-		
-		JButton nextBreed = new JButton(">");
-		nextBreed.setFont(new Font("Verdana", Font.PLAIN, 78));
-		nextBreed.setBounds(1144, 274, 100, 100);
-		viewBreed.add(nextBreed);
-		nextBreed.setBackground(Color.WHITE);
-		
-		
-		addBreed = new JPanel();
-		addBreed.setBounds(0, 0, 1280, 691);
-		layeredPane.add(addBreed);
-		addBreed.setVisible(false);
-		addBreed.setBackground(Color.WHITE);
-		addBreed.setLayout(null);
-		
-		JPanel menu = new JPanel();
-		menu.setBounds(0, 0, 1270, 691);
-		layeredPane.add(menu);
+		//menu		
+		menu = new JPanel();
+		menu.setBackground(Color.WHITE);
+		layeredPane.add(menu, "name_410359960271086");
 		menu.setLayout(null);
 		
 		JButton btnBrowse = new JButton("Browse breeds");
+		btnBrowse.setBounds(100, 300, 400, 200);
+		btnBrowse.setFont(new Font("Verdana", Font.PLAIN, 40));
+		btnBrowse.setBorder(new LineBorder(Color.DARK_GRAY));
+		btnBrowse.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(5.0f)));
+		btnBrowse.setBackground(Color.WHITE);		
+		btnBrowse.setRequestFocusEnabled(false);
+		btnBrowse.setVisible(true);
+		btnBrowse.setFocusPainted(false);
+		btnBrowse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				layeredPane.removeAll();
+				layeredPane.add(viewBreed);
+				layeredPane.repaint();
+				layeredPane.revalidate();
+				setText();
+				setBreed();
+			}
+		});
+		
 		btnBrowse.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
@@ -151,31 +195,134 @@ public class Window {
 				btnBrowse.setBackground(Color.WHITE);
 			}
 		});
-		btnBrowse.setBounds(100, 300, 400, 200);
-		btnBrowse.setFont(new Font("Verdana", Font.PLAIN, 40));
-		btnBrowse.setBorder(new LineBorder(Color.DARK_GRAY));
-		btnBrowse.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(5.0f)));
-		btnBrowse.setBackground(Color.WHITE);
-		btnBrowse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				viewBreed.setVisible(true);
-			}
-		});
 		menu.add(btnBrowse);
 		
+		addBreed = new JPanel();
+		layeredPane.add(addBreed, "name_410359942089403");
+		addBreed.setVisible(false);
+		addBreed.setBackground(Color.WHITE);
+		addBreed.setLayout(null);	
+		
+		
+		
+		
+		
+		
+		//view breed window
+		viewBreed = new JPanel();
+		layeredPane.add(viewBreed, "name_410359924014670");
+		viewBreed.setLayout(null);
+		viewBreed.setVisible(false);
+		viewBreed.setBackground(Color.WHITE);
+		
+		
+		ImageIcon previous = new ImageIcon("src/images/previous.png");
+		ImageIcon previousHover = new ImageIcon("src/images/previousHover.png");
+
+		JButton prevBreed = new JButton(previous);
+		prevBreed.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				prevBreed.setIcon(previousHover);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				prevBreed.setIcon(previous);
+			}
+		});
+		prevBreed.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				View.changeBreed(false);
+			}
+		});
+		prevBreed.setBounds(30, 300, previous.getIconHeight(), previous.getIconWidth());
+		viewBreed.add(prevBreed);
+		prevBreed.setRequestFocusEnabled(false);
+		prevBreed.setOpaque(false);
+		prevBreed.setContentAreaFilled(false);
+		prevBreed.setBorderPainted(false);
+		prevBreed.setFocusPainted(false);
+		
+
+		ImageIcon next = new ImageIcon("src/images/next.png");
+		ImageIcon nextHover = new ImageIcon("src/images/nextHover.png");
+		
+		JButton nextBreed = new JButton(next);
+		nextBreed.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				nextBreed.setIcon(nextHover);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				nextBreed.setIcon(next);
+			}
+		});
+		nextBreed.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				View.changeBreed(true);
+			}
+		});
+		nextBreed.setBounds(1140, 300, previous.getIconHeight(), previous.getIconWidth());
+		viewBreed.add(nextBreed);
+		nextBreed.setRequestFocusEnabled(false);
+		nextBreed.setVisible(true);
+		nextBreed.setOpaque(false);
+		nextBreed.setContentAreaFilled(false);
+		nextBreed.setBorderPainted(false);
+		nextBreed.setFocusPainted(false);
+		
+		
+		
+		
+		//add breed window
 		JButton btnAdd = new JButton("Add new breed");
 		btnAdd.setBounds(780, 300, 400, 200);
 		btnAdd.setFont(new Font("Verdana", Font.PLAIN, 40));
 		btnAdd.setBorder(new LineBorder(Color.DARK_GRAY));
 		btnAdd.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(5.0f)));
 		btnAdd.setBackground(Color.WHITE);		
+		btnAdd.setRequestFocusEnabled(false);
+		btnAdd.setFocusPainted(false);
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				addBreed.setVisible(true);
+				layeredPane.removeAll();
+				layeredPane.add(addBreed);
+				layeredPane.repaint();
+				layeredPane.revalidate();
+			}
+		});
+		
+		btnAdd.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnAdd.setBackground(new Color(237, 237, 237));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnAdd.setBackground(Color.WHITE);
 			}
 		});
 		menu.add(btnAdd);
+		
+
+
+		
+		breedLabels = new JLabel[breedInfo.length];
+
+		for(int i=0; i<breedInfo.length; i++) {
+			breedLabels[i] = new JLabel(breedInfo[i]);
+		}
+		
+		
+		textLabels = new JLabel[breedInfo.length];
+
+		for(int i=0; i<breedInfo.length; i++) {
+			textLabels[i] = new JLabel(breedInfo[i]);
+		}
+
 
 		
 	}
+
 }
